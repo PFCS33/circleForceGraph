5844c9db94f5a84ebde63e92ced508dbf59b56f5
pfcs33 <aaapfcs@gmail.com>
2024-03-15 14:27:31 +0800

    bugfix: panel communication

--- src/components/circle-graph/InfoPanel.vue
+++ src/components/circle-graph/InfoPanel.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="panel-container"></div>
+  <div
+    class="panel-container"
+    v-loading="isLoading"
+    element-loading-text="Fetching data..."
+  ></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { watch, ref, onMounted } from "vue";
+import { baseUrl, postData } from "@/utils/api.js";
+const props = defineProps({
+  id: Number,
+});
+
+/* -------------------------------------------------------------------------- */
+// communicate with backend server
+/* -------------------------------------------------------------------------- */
+const isLoading = ref(true);
+const panelData = ref(null);
+// communicate to server to get other insights in the same data scope
+const postFunc = async (id) => {
+  try {
+    isLoading.value = true;
+    const data = await postData(baseUrl + "/panel/data", {
+      id: id,
+    });
+    panelData.value = data;
+    isLoading.value = false;
+  } catch (e) {
+    ElMessage.error(`Panel Error: ${e.message}`);
+  }
+};
+
+watch(panelData, (newVal, oldVal) => {
+  console.log("watch", props.id);
+  console.log(newVal, oldVal);
+});
+
+onMounted(() => {
+  postFunc(props.id);
+});
+</script>
 
 <style lang="scss" scoped>
 .panel-container {
-  width: 20rem;
+  width: 30rem;
   height: 100%;
   z-index: $z-middle;
 
